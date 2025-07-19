f1fb11dc461d20ce0af01c35287b25949f565127
maru3172 <96215725+maru3172@users.noreply.github.com>
2025-07-19 23:57:44 +0900

    First Work

---
+++ Source/GoodGame/GoodGame.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GoodGame : ModuleRules
+{
+	public GoodGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"GoodGame",
+			"GoodGame/Variant_Platforming",
+			"GoodGame/Variant_Combat",
+			"GoodGame/Variant_Combat/AI",
+			"GoodGame/Variant_SideScrolling",
+			"GoodGame/Variant_SideScrolling/Gameplay",
+			"GoodGame/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/GoodGame/GoodGameGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GoodGameGameMode.h"
+
+AGoodGameGameMode::AGoodGameGameMode()
+{
+	// stub
+}
